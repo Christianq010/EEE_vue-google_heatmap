c578841f4c58ae52692e4e7374b05719c4990a94
Christiaan Quyn <christianq010@gmail.com>
2018-11-03 19:01:49 +0530

    initial commit - Add vue google heat maps and render heat map

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueGoogleHeatmap from 'vue-google-heatmap';
 
 Vue.config.productionTip = false
+
+
+Vue.use(VueGoogleHeatmap, {
+  apiKey: 'AIzaSyD2tKBjRkdAtSJthU2Wm135MmtptdoQxm8'
+});
 
 new Vue({
   render: h => h(App),
