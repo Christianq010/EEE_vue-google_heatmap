02e193b9eb4ca8a8730f3f9b5de8aa7fcd68967f
Christiaan Quyn <christianq010@gmail.com>
2018-11-15 15:34:36 +0530

    Feat: Render firebase response on app

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/DMU-logo.jpg" class="logo"> -->
     <Map />
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="signal in signals" v-bind:key="signal.timestamp">
+      <p>{{signal.latitude}}</p>
+    </div>
+    <HelloWorld msg="Telecom Signal Strength Data App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Map from './components/Map.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Map from "./components/Map.vue";
+import Firebase from "firebase";
+let config = {
+  apiKey: "AIzaSyDUDOGJNvWpaZ2BrIDJny4vgVfJl7cOG3U",
+  authDomain: "fieldtest-b5a57.firebaseapp.com",
+  databaseURL: "https://fieldtest-b5a57.firebaseio.com",
+  projectId: "fieldtest-b5a57",
+  storageBucket: "fieldtest-b5a57.appspot.com",
+  messagingSenderId: "390084766159"
+};
+
+let app = Firebase.initializeApp(config);
+let db = app.database();
+let signalRef = db.ref("database");
+
+// JSON at "https://fieldtest-b5a57.firebaseio.com/database.json";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld,
     Map
+  },
+  firebase: {
+    signals: signalRef
+  },
+  data() {
+    return {
+      newSignal: {
+        latitude: "",
+        longitude: "",
+        signalStrength: ""
+      }
+    };
   }
-}
+};
+console.log(signalRef);
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.logo {
+  width: 250px;
+  height: 250px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueGoogleHeatmap from 'vue-google-heatmap';
+import Vue from "vue";
+import App from "./App.vue";
+import VueGoogleHeatmap from "vue-google-heatmap";
+import VueFire from "vuefire";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+Vue.use(VueFire);
 
 Vue.use(VueGoogleHeatmap, {
-  apiKey: 'AIzaSyD2tKBjRkdAtSJthU2Wm135MmtptdoQxm8'
+  apiKey: "AIzaSyD2tKBjRkdAtSJthU2Wm135MmtptdoQxm8"
 });
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
